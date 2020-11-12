6c09469420eca9119be9f155cb19ed76b723f3c2
Affian <hellriser66@gmail.com>
2020-11-12 21:44:49 +0800

    First commit

---
+++ src/main/java/com/argosenpaikun/risa/utils/data/DelimiterException.java
@@ -0,0 +1,6 @@
+package com.argosenpaikun.risa.utils.data;/**
+ * Author: Mohamed Affian bin Che Mohamed Onn
+ * Date: 12/11/2020
+ * Company: Dataran Teknologi Sdn. Bhd.
+ */public class DelimiterException {
+}
